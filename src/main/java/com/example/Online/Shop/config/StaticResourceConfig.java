4e94dc79c9af59ff7586e7581484460088daa8a8
Jebimeta <jebilongo@gmail.com>
2025-04-01 18:07:56 +0200

    Added StaticResourceConfig

---
+++ src/main/java/com/example/Online/Shop/config/StaticResourceConfig.java
@@ -0,0 +1,21 @@
+package com.example.Online.Shop.config;
+
+import com.example.Online.Shop.config.properties.OnlineShopProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class StaticResourceConfig implements WebMvcConfigurer {
+
+    private final OnlineShopProperties properties;
+
+    @Override
+    // Con este recurso manejamos imagenes cuando reciba peticiones de los productos o imagenes
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/app/static/products/images/**", "/products/images/**")
+                .addResourceLocations("file:/app/static/products/images/", properties.getUploadProperties().getLocalDirectory());
+    }
+}
